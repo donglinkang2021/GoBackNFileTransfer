4f081156057a3dff06252c0a9ed8bd2d03746f11
Linkdom <donglinkang2021@163.com>
2024-04-13 23:50:17 +0800

    add crc dev ver 0.0

--- crc.py
+++ crc.py
@@ -1,37 +1,36 @@
 """
-CRC order: 16
-CRC polynomial: 0x1021
-Inital value: 0xFFFF
-Final value: 0x0000
-Direct: True
+crc demo here
 """
-def crc16(data : bytearray, offset:int , length:int):
-    if data is None or offset < 0 or offset > len(data)- 1 and offset+length > len(data):
-        return 0
-    crc = 0xFFFF
-    print("crc: ")
-    print("- bin: ", bin(crc))
-    print("- hex: ", hex(crc))
-    for i in range(0, length):
-        print("data:")
-        print("- bin: ", bin(data[offset + i]))
-        print("- hex: ", hex(data[offset + i]))
-        print("data[offset + i] << 8: ")
-        print("- bin: ", bin(data[offset + i] << 8))
-        print("- hex: ", hex(data[offset + i] << 8))
-        crc ^= data[offset + i] << 8
-        print("crc: ")
-        print("- bin: ", bin(crc))
-        print("- hex: ", hex(crc))
-        for j in range(0,8):
-            if (crc & 0x8000) > 0:
-                crc =(crc << 1) ^ 0x1021
-            else:
-                crc = crc << 1
-    return crc & 0xFFFF
 
-# test
-data = bytearray(b'\x01\x02\x03\x04')
+data = 0b0010_1001
+original_data = data
+poly = 0b1101
 
-# show the result
-print(hex(crc16(data, 0, len(data)))) # 0x31c3
+def bit_length(x):
+    return len(bin(x)) - 2
+
+data_len = bit_length(data)
+crc_len = bit_length(poly) - 1
+total_length = data_len + crc_len
+
+data <<= crc_len
+print(f"Data: {bin(data)}")
+
+
+quotient = []
+for i in range(data_len):
+    print(f"mask: {bin(1 << (total_length - 1 - i))}")
+    if data & (1 << (total_length - 1 - i)):
+        quotient.append("1")
+        print(f"poly: {bin(poly << (data_len - 1 - i))}")
+        data ^= poly << (data_len - 1 - i)
+        print(f"Data: {bin(data)}")
+    else:
+        quotient.append("0")
+
+print(f"Quotient: {''.join(quotient)}")
+print(f"Remainder: {data:0{crc_len}b}")
+
+print(f"Transmitted data: {bin(original_data << crc_len | data)}")
+
+
