f08a825a2031e621e4ad1b761eac07af91561655
Linkdom <donglinkang2021@163.com>
2024-04-13 20:29:22 +0800

    code send file to vm

--- dev/client.py
+++ dev/client.py
@@ -4,20 +4,15 @@ from tqdm import tqdm
 
 SEPERATOR = "<SEP>"
 
-# get host 
-hostname = socket.gethostname()
-host = socket.gethostbyname(hostname)
-print(f"host: {host}")
-print(f"hostname: {hostname}")
-
-# set port
+# get server host 
+host = "192.168.10.129"
 port = 42477
 
 # buffer for file transfer
 BUFFER_SIZE = 4096
 
 # file path
-file_path = "file_examples/1_chpt1_Introduction-2024.pdf"
+file_path = "file_examples/miziha_running.png"
 # get file size
 file_size = os.path.getsize(file_path)
 print(f"file size: {file_size / 1024 / 1024:.2f} MB")
@@ -45,8 +40,8 @@ with open(file_path, "rb") as file:
 
         # update the progress bar
         progress.update(len(bytes_read))
-
+progress.close()
 # close the socket
 client_socket.close()
 print("file has been sent successfully")
-progress.close()
+
