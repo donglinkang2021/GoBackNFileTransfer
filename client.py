db6ab3a3fde63b1237c20948378152c74463f965
Linkdom <donglinkang2021@163.com>
2024-04-13 20:01:00 +0800

    add simple file transfer code

--- client.py
+++ client.py
@@ -1,17 +1,52 @@
 import socket
+import os
+from tqdm import tqdm
 
-# 创建 socket 对象
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SEPERATOR = "<SEP>"
 
-# 获取本地主机名
-host = socket.gethostname()
-port = 12345  # 设置端口号
+# get host 
+hostname = socket.gethostname()
+host = socket.gethostbyname(hostname)
+print(f"host: {host}")
+print(f"hostname: {hostname}")
 
-# 连接服务，指定主机和端口
+# set port
+port = 42477
+
+# buffer for file transfer
+BUFFER_SIZE = 4096
+
+# file path
+file_path = "file_examples/1_chpt1_Introduction-2024.pdf"
+# get file size
+file_size = os.path.getsize(file_path)
+print(f"file size: {file_size / 1024 / 1024:.2f} MB")
+
+# creare a socket object
+client_socket = socket.socket()
+print(f"connecting to {host}:{port}")
 client_socket.connect((host, port))
+print("connected")
 
-# 接收服务器消息
-msg = client_socket.recv(1024)
-print(msg.decode('utf-8'))
+# send the filename and filesize
+client_socket.send(f"{file_path}{SEPERATOR}{file_size}".encode())
 
-client_socket.close()  # 关闭连接
+# start sending the file
+progress = tqdm(range(file_size), f"sending {file_path}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(file_path, "rb") as file:
+    while True:
+        # read the bytes from the file
+        bytes_read = file.read(BUFFER_SIZE)
+        if not bytes_read:
+            # file transmitting is done
+            break
+        # we use sendall to assure transimission in busy networks
+        client_socket.sendall(bytes_read)
+
+        # update the progress bar
+        progress.update(len(bytes_read))
+
+# close the socket
+client_socket.close()
+print("file has been sent successfully")
+progress.close()
--- server.py
+++ server.py
@@ -1,28 +1,57 @@
 import socket
+import os
+from tqdm import tqdm
 
-# 创建 socket 对象
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SEPERATOR = "<SEP>"
 
-# 获取本地主机名
-host = socket.gethostname()
-print(f"本地主机名: {host}")
-port = 12345  # 设置端口号
+SERVER_HOST = "192.168.56.1"
+SERVER_PORT = 42477
 
-# 绑定端口号
-server_socket.bind((host, port))
+BUFFER_SIZE = 4096 
 
-# 设置最大连接数，超过后排队
+# create the server socket
+server_socket = socket.socket()
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+# make the server listen to new connections
 server_socket.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
-print(f"服务器启动，监听来自 {host} 的连接，端口号 {port}...")
+# accept connection if there is any
+client_socket, address = server_socket.accept()
+# if below code is executed, that means the sender is connected
+print(f"[+] {address} is connected.")
 
-while True:
-    # 建立客户端连接
-    client_socket, addr = server_socket.accept()
+# receive the file infos
+# receive using client socket, not server socket
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPERATOR)
 
-    print(f"连接地址: {str(addr)}")
+# remove absolute path if there is
+filename = os.path.basename(filename)
+filesize = int(filesize)
 
-    msg = '欢迎访问服务器！' + "\r\n"
-    client_socket.send(msg.encode('utf-8'))
+# start receiving the file from the socket
+# and writing to the file stream
+progress = tqdm(range(filesize), f"Receiving {filename}", unit="B", 
+                unit_scale=True, unit_divisor=1024)
 
-    client_socket.close()  # 关闭连接
+with open(filename, "wb") as file:
+    while True:
+        # read 1024 bytes from the socket (receive)
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:
+            # nothing is received
+            # file transmitting is done
+            break
+        # write to the file the bytes we just received
+        file.write(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+progress.close()
+
+# close the client socket
+client_socket.close()
+# close the server socket
+server_socket.close()
+
