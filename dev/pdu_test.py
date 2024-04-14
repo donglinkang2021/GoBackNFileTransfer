a521f64b64f35c8c2f6c906afdefa80dc9337f4f
Linkdom <donglinkang2021@163.com>
2024-04-14 16:49:56 +0800

    code pdu

---
+++ dev/pdu_test.py
@@ -0,0 +1,34 @@
+import struct
+from pdu import PDU
+
+frame_no = 1
+ack_no = 2
+
+integer_value = 42
+float_value = 3.14
+byte_value = b'\x00\x01\x02\x03'
+data = struct.pack('if4s', integer_value, float_value, byte_value)
+data_size = len(data)
+
+pdu = PDU(frame_no, ack_no, data_size, data)
+print(pdu)
+
+
+packed_data = pdu.pack()
+
+# Simulate data corruption
+print('---Simulate data corruption---')
+print(packed_data)
+# change one byte at index 5
+packed_data = packed_data[:5] + b'\x01' + packed_data[6:] 
+print(packed_data)
+
+unpacked_pdu = PDU.unpack(packed_data)
+if unpacked_pdu.is_corrupted():
+    print('PDU is corrupted')
+else:
+    print("PDU is not corrupted")
+    print(unpacked_pdu)
+    integer_value, float_value, byte_value = struct.unpack('if4s', unpacked_pdu.data)
+    print(integer_value, float_value, byte_value)
+
---
+++ pdu.py
@@ -0,0 +1,37 @@
+import struct
+from crc import CRC, CRC_ALGORITHMS
+
+crc16 = CRC(**CRC_ALGORITHMS["CRC-16/CCITT"])
+
+class PDU:
+    def __init__(self, frame_no:int, ack_no:int, data_size:int,
+                 data:bytes, checksum:int = None):
+        self.frame_no = frame_no
+        self.ack_no = ack_no
+        self.data_size = data_size
+        self.data = data
+        self.checksum = checksum
+
+    def __str__(self):
+        return f'frame_no: {self.frame_no}, ack_no: {self.ack_no}, data_size: {self.data_size}, checksum: {self.checksum}'
+    
+    def get_packed_data(self):
+        return struct.pack('HHH', self.frame_no, self.ack_no, self.data_size) + self.data
+
+    def pack(self):
+        packed_data = self.get_packed_data()
+        self.checksum = crc16.calculate(packed_data)
+        packed_data += struct.pack('H', self.checksum)
+        return packed_data
+    
+    def is_corrupted(self):
+        packed_data = self.get_packed_data()
+        return crc16.calculate(packed_data) != self.checksum
+    
+    @staticmethod
+    def unpack(packed_data:bytes):
+        frame_no, ack_no, data_size = struct.unpack('HHH', packed_data[:6])
+        data = packed_data[6:-2]
+        checksum = struct.unpack('H', packed_data[-2:])[0]
+        return PDU(frame_no, ack_no, data_size, data, checksum)
+
